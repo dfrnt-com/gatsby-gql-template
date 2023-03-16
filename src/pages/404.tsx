8092318672f1fc28d4aeb5f56931c355de04b351
Philippe Höij <philippe@hoij.net>
2023-03-16 18:35:56 +0100

    Updated 404 page too

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,49 +1,37 @@
-import * as React from "react"
-import { Link, HeadFC, PageProps } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import * as React from "react";
+import { Link, HeadFC, PageProps } from "gatsby";
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
+    <main className="bg-white">
+      <div className="overflow-hidden bg-white py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto grid max-w-2xl grid-cols-1 gap-y-16 gap-x-8 sm:gap-y-20">
+            <div className="lg:pr-8 lg:pt-4">
+              <div className="text-center">
+                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Page not found</h1>
+                <p className="mt-6 text-lg leading-8 text-gray-600">Sorry 😔, we couldn’t find what you were looking for.</p>
+                <br />
+                {process.env.NODE_ENV === "development" ? (
+                  <>
+                    <br />
+                    Try creating a page in <code>src/pages/</code>.
+                    <br />
+                  </>
+                ) : null}
+                <br />
+                <div className="mt-10 flex items-center justify-center gap-x-6">
+                  <Link to="/">Go home</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
 
-export const Head: HeadFC = () => <title>Not found</title>
+export const Head: HeadFC = () => <title>Not found</title>;
