d1f0538e53f51463488e6eea7515c155303321e6
Philippe Höij <philippe@hoij.net>
2023-03-19 00:36:33 +0100

    Fix for React 18 types

--- src/layouts/BaseLayout/BaseLayout.tsx
+++ src/layouts/BaseLayout/BaseLayout.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-const BaseLayout: React.FC = ({children}): JSX.Element => (
+const BaseLayout: React.FC<React.PropsWithChildren> = ({children}): JSX.Element => (
   <main className="bg-white">
     <div className="overflow-hidden bg-white py-16 sm:py-24">
       <div className="max-w-7xl mx-4 md:mx-16 px-6 lg:px-8">
